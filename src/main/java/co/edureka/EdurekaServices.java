9c8b31c63149468a0b84185e6dfe97c776f22d56
callgp <callgp@gmail.com>
2020-11-21 17:09:35 -0500

    rest path param

--- src/main/java/co/edureka/EdurekaServices.java
+++ src/main/java/co/edureka/EdurekaServices.java
@@ -2,6 +2,7 @@ package co.edureka;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("edureka")
@@ -23,6 +24,15 @@ public class EdurekaServices {
 		public String nriService(){
 			return "<h2>nri customer service center</h2>";
 		}
+		
+		
+		@GET
+		@Path("{name}/{course}")
+		@Produces("text/html")
+		public String enquiry(@PathParam("name") String name,@PathParam("course") String course){
+			return " customer name" +name+ "course is" +course;
+		}
+		
 //	}
 
 }
